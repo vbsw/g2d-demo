a1526b4968d6a35273dcaecfcefcea38a6f0f043
Vitali Baumtrok <vbsw@mailbox.org>
2022-06-27 11:53:48 +0200

    more functions

--- window.go
+++ window.go
@@ -8,6 +8,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/vbsw/g2d"
 )
 
@@ -20,6 +21,36 @@ func newDemoWindow(params *tParameters) *tDemoWindow {
 	window := new(tDemoWindow)
 	window.params = params
 	return window
+}
+
+func (window *tDemoWindow) Create() error {
+	fmt.Println("create")
+	return nil
+}
+
+func (window *tDemoWindow) Show() error {
+	fmt.Println("show")
+	return nil
+}
+
+func (window *tDemoWindow) KeyDown(key int, repeated uint) error {
+	if repeated == 0 {
+		if key == 41 { // esc
+			window.Cmd.CloseReq = true
+		} else if key == 9 { // f
+			window.Props.Fullscreen = !window.Props.Fullscreen
+		} else if key == 23 { // t
+			fmt.Println("time", window.Time.NanosEvent)
+		} else {
+			fmt.Println("key down", key)
+		}
+	} else {
+	}
+	return nil
+}
+
+func (window *tDemoWindow) KeyUp(key int) error {
+	return nil
 }
 
 func (window *tDemoWindow) Close() (bool, error) {
